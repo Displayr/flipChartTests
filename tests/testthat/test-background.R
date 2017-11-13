998d686084700b75cc187cbfc072fb8687dd42e9
chschan <chschan@gmail.com>
2017-11-13 16:47:07 +1100

    DS-1480: More comprehensive tests for scatter and chart backgrounds

--- tests/testthat/test-background.R
+++ tests/testthat/test-background.R
@@ -1,37 +1,68 @@
-context("Background snapshots")
-library("flipStandardCharts")
-    
-funcs <- c("Column", "Bar", "Area", "Line")
+context("Chart backgrounds")
 
-# Add named entries to test.args to add more snapshot tests
-# Here we are checcking the behaviour of the chart background and axis
+# Here we are checking the behaviour of the plotly chart background and axis
+# Only checking charting functions supporting most of plotly chart axes options
+# Corresponding tests for Radar charts are in their own file
+funcs <- c("Column", "Bar", "Area", "Line", "Scatter")
+
 test.args <- c('default' = '',
+    'datalabelonly' = 'data.label.show=TRUE, y.tick.show=FALSE',
+    'zeros' = 'x.zero=TRUE, y.zero=TRUE, x.zero.line.color="red", y.zero.line.color="blue"',
+    'zerolabeled' = 'x.zero=TRUE, y.zero=TRUE, x.zero.line.color="red", y.zero.line.color="blue", data.label.show=TRUE',
+    'zerorev' = 'x.zero=TRUE, y.zero=TRUE, x.zero.line.color="red", y.zero.line.color="blue", x.data.reversed=TRUE, y.data.reversed=TRUE',
+    'zerorevlabeled' = 'x.zero=TRUE, y.zero=TRUE, x.zero.line.color="red", y.zero.line.color="blue", data.label.show=T, x.data.reversed=TRUE, y.data.reversed=TRUE',
+    'zerorevfit' = 'x.zero=TRUE, y.zero=TRUE, x.zero.line.color="red", y.zero.line.color="blue", fit.type = "Smooth", x.data.reversed=TRUE, y.data.reversed=TRUE',
+    'fitlabeled' = 'fit.type = "Smooth", data.label.show = TRUE',
     'backgroundcolors' = 'background.fill.color="grey", charting.area.fill.color="yellow", charting.area.fill.opacity=0.2',
-    'grid' = 'x.line.width=2, y.line.width=4, y.line.color="red", x.line.color="blue", y.tick.mark.length=10, x.tick.mark.length=1, x.grid.width=1, y.grid.width=1',
+    'grid' = 'x.line.width=2, y.line.width=4, y.line.color="red", x.line.color="blue", y.tick.mark.length=10, x.tick.mark.length=1, x.grid.width=4, y.grid.width=1',
+    'nogrid' = 'grid.show=FALSE, x.grid.width=1, y.grid.width=1',
     'tickdist' = 'y.bounds.minimum=3, y.bounds.maximum=20, y.tick.distance=1',
+    'ticklabels' = 'y.tick.prefix="<", y.tick.suffix=">"',
     'reversed' = 'x.data.reversed=TRUE, y.data.reversed=TRUE',
-    'legendpos' = 'legend.position.y=0.5, legend.position.x=0, legend.font.color="red"',
-    'margins' = 'margin.left=0, margin.right=0, margin.top=0, margin.inner.pad=10, charting.area.fill.color="red", legend.show=FALSE, grid.show=FALSE',
-    'font' = 'global.font.family="Courier", global.font.color="red"')
+    'legendpos' = 'legend.position.y=0.5, legend.position.x=0, legend.font.color="red", legend.fill.opacity=0',
+    'legendbg' = 'legend.fill.color="blue", legend.fill.opacity=0.5, legend.border.color="red", legend.border.line.width=2',
+    'margins' = 'margin.left=0, margin.right=0, margin.top=0, margin.inner.pad=10, margin.bottom=0, background.fill.color="blue", charting.area.fill.color="red", legend.show=FALSE, grid.show=FALSE',
+    'font' = 'global.font.family="Courier", global.font.color="red"',
+    'modebar' = 'modebar.show = TRUE')
 
-dat1 <- matrix(c(1:20), 10, 2, dimnames=list(letters[1:10], c("X", "Y")))
+# data set for each axis type
+positives <- matrix(c(1:20), 10, 2, dimnames=list(1:10 + 5, c("X", "Y")))
+negatives <- matrix(c(1:20), 10, 2, dimnames=list(1:10 - 20, c("X", "Y")))
+categoricals <- matrix(c(1:20), 10, 2, dimnames=list(letters[1:10], c("X", "Y")))
+dates <- matrix(c(1:20), 10, 2,
+                dimnames=list(sprintf("%02d/01/2017", c(1:5, 16:20)), c("X", "Y")))
 
+# corresponding data sets for scatterplot
+sc.positives <- 1:10 + 5
+sc.negatives <- 1:10 - 20
+sc.categoricals <- data.frame(Class=factor(sample(LETTERS[1:3], 10, replace = TRUE)), Y=1:10)
+sc.dates <- data.frame(Date=as.Date("2017-01-01") + c(0:4, 15:19), Y=1:10)
+
+dat.list <- c("positives", "negatives", "categoricals", "dates")
 for (ff in funcs)
 {
-    for (i in 1:length(test.args))
+    for (dat in dat.list)
     {
-        # filestem is both the name of the image in accepted-snapshots
-        # and the error msg expected on the output of devtools::test()
-        filestem <- paste0("background-", tolower(ff), "-", names(test.args)[i])
-        if (filestem %in% c("background-radar-reversed", "background-radar-grid"))
-            next
-        test_that(filestem, {
-            extra.args <- ""
-            if (filestem == "background-scatter-legendpos")
-                extra.args <- ", scatter.colors.column=2, scatter.colors.as.categorical=T"
-            cmd <- paste0("pp <- ", ff, "(dat1, ", test.args[i], extra.args, ")")
-            expect_error(eval(parse(text=cmd)), NA)
-            expect_true(TestWidget(pp, filestem))
-        })
+        for (i in 1:length(test.args))
+        {
+            # filestem is both the name of the image in accepted-snapshots
+            # and the error msg expected on the output of devtools::test()
+            filestem <- paste("background", tolower(ff), dat,
+                              names(test.args)[i], sep="-")
+
+            if (grepl("bar-.*-ticklabel", filestem))
+                next
+            test_that(filestem, {
+
+                if (ff == "Scatter")
+                    dat <- paste0("sc.", dat)
+                cmd <- paste0("pp <- ", ff, "(", dat, ",", test.args[i], ")")
+                expect_error(eval(parse(text=cmd)), NA)
+
+                expect_true(TestWidget(pp, filestem))
+                #print(pp)
+                #readline(prompt=paste0(filestem, ": press [enter] to continue: "))
+            })
+        }
     }
 }
--- tests/testthat/test-scatter.R
+++ tests/testthat/test-scatter.R
@@ -1,39 +1,35 @@
-context("Scatter")
+context("Scatter plot")
 library("flipChartBasics")
 library("flipStandardCharts")
 
 # Set up dataframe containing different types of data types
-# None with missing values???
 set.seed(1234)
 dat <- data.frame('Score' = rnorm(20),
                   'Cost ($)' = abs(rnorm(20)), # check plotly is handling '$' properly
                   'Age' = rpois(20, 40),
-                  'Class' = factor(sample(LETTERS[4:1], 20, replace = TRUE)),
-                  'Desc' =  stringi::stri_rand_strings(20, 5),
-                  #'Sporadic' = c(1, NA, rpois(17, 2), NA),
-                  'Date' = as.Date(sprintf("2017-01-%02d", 1:20)),
-                  check.names = FALSE, stringsAsFactors = FALSE)
+                  'Class' = factor(sample(LETTERS[4:1], 20, replace = TRUE), levels = LETTERS[4:1]), # reverse order to check DS-1645
+                  'Sporadic' = c(1:5, NA, 6:10, NA, NA, 11:12, NA, NA, 13:15), # missing values
+                  'Date' = as.Date(sprintf("2017-01-%02d", 20:1)),
+                   check.names = FALSE, stringsAsFactors = FALSE)
 rownames(dat) <- letters[1:20]
 
 # Set up matrix to use the different variable types
-#tmp <- expand.grid(0:6, 0:6, 0:6, 0:6)
-tmp.attr <- expand.grid(0:6, 0:6)
-tmp.var <- t(combn(0:6, 2))
-tmp <- cbind(tmp.var[c(1:21, 1:21, 1:7),], tmp.attr)
+grid2 <- expand.grid(0:6, 0:6)
+tmp <- cbind(rbind(c(NA, 6), c(6, NA), grid2[2:49,]), grid2[c(1, 49:1),])
 
-columns.str <- sprintf("scatter.x.column = %d, scatter.y.column = %d,
-                       scatter.colors.column = %d, scatter.sizes.column = %d",
+columns.str <- sprintf("scatter.x.column = %.0f, scatter.y.column = %d,
+                        scatter.colors.column = %d, scatter.sizes.column = %d",
                        tmp[,1], tmp[,2], tmp[,3], tmp[,4])
 names(columns.str) <- apply(tmp, 1, paste, collapse="")
 
 # These are only the options that can be used by both Labeled and (plotly) Scatterplots
 # Line of best fit is already tested in test-backgrounds.R
 opts <- c('default' = 'colors = ChartColors(5, "Blues")',
-          'categoricalcolor' = 'scatter.colors.as.categorical = TRUE, legend.font.color = "red"',
-          'numericalcolor' = 'scatter.colors.as.categorical = FALSE, colors = grey(1:4/5)',
-          'nolegend' = 'legend.show = FALSE, colors = "red"',
-          'markerbig' = 'series.marker.size = 20, grid.show = FALSE',
-          'thickxgrid' = 'x.grid.width = 10, global.font.color = "red", global.font.family = "Courier"')
+         'categoricalcolor' = 'scatter.colors.as.categorical = TRUE, legend.font.color = "red"',
+         'numericalcolor' = 'scatter.colors.as.categorical = FALSE, colors = grey(1:4/5)',
+         'nolegend' = 'legend.show = FALSE, colors = "red"',
+         'markerbig' = 'series.marker.size = 20, grid.show = FALSE',
+         'thickxgrid' = 'x.grid.width = 10, global.font.color = "red", global.font.family = "Courier"')
 
 n <- length(opts)
 index <- 1
@@ -44,12 +40,14 @@ for (func in c("Scatter", "LabeledScatter"))
         jj <- n - (index %% n)
         filestem <- paste0(tolower(func), "-", names(columns.str)[ii], "-", names(opts)[jj])
         test_that(filestem, {
-            
+
             cmd <- paste0("pp <- ", func, "(dat, ", columns.str[ii], ", ", opts[jj], ")")
             expect_error(suppressWarnings(eval(parse(text = cmd))), NA)
-            expect_true(TestWidget(pp, filestem))    
+
+            #print(pp)
+            #readline(prompt=paste0(filestem, ": press [enter] to continue: "))
+            expect_true(TestWidget(pp, filestem))
         })
         index <- index + 1
     }
 }
-
