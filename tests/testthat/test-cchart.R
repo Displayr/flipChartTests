39db58486728c4eeda838eefc4f9725e5745f276
chschan <chschan@gmail.com>
2019-03-04 12:05:51 +1100

    Accept more diffs

--- tests/testthat/test-cchart.R
+++ tests/testthat/test-cchart.R
@@ -9,8 +9,11 @@ for (func in c("Column", "Bar", "Distribution"))
 {
     filestem <- paste("cchart", tolower(func), "axistitle", sep="-")
     test_that(filestem, {
-        expect_error(pp <- suppressWarnings(CChart("Column", xx, categories.title = "Categories", 
+        expect_error(pp <- suppressWarnings(CChart(func, xx, categories.title = "Categories", 
                                   values.title = "Values")), NA)
-        expect_true(TestWidget(pp, filestem))
+        expect_true(TestWidget(pp, filestem))     
+        
+        #print(pp)
+        #readline(prompt=paste0(filestem, ": press [enter] to continue: "))
     })
 }
