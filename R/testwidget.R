25f108102507a72dc90513729a929bb009ceb19b
chschan <chschan@users.noreply.github.com>
2022-04-09 00:04:10 +1000

    Try increasing default delay before snapshot is taken to get hover showing

--- R/testwidget.R
+++ R/testwidget.R
@@ -14,7 +14,7 @@
 #' @export
 TestWidget <- function(widget, 
                        name,
-                       delay = 0.2,
+                       delay = 2,
                        threshold = 0.001,
                        ...,
                        diff.path = "snapshots/diff",
