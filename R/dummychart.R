96bd7563901742351a3f568b63594a847f5cda94
chschan <carmen.chan@displayr.com>
2025-04-04 13:56:09 +1100

    Rexport function to keep dependencies intact

--- R/dummychart.R
+++ R/dummychart.R
@@ -1,6 +1,12 @@
 #' @importFrom flipStandardCharts Column
-#' @importFrom flipSnapshotTestUtils TestWidget CompareSnapshot CreateSnapshot
+#' @keywords internal
 CreateDummyChart <- function()
 {
     Column(1:10)
 }
+
+#' @importFrom flipSnapshotTestUtils TestWidget CreateSnapshot CompareSnapshot
+#' @rdname reexports
+#' @title Function reexported from flipSnapshotTestUtils
+#' @export
+flipSnapshotTestUtils::TestWidget
